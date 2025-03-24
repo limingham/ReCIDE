cccd597b9b0580ed2979e7725dbc26ef3153569d
limingham <160384038+limingham@users.noreply.github.com>
2025-03-24 11:19:45 +0800

    Update ReCIDE_all_function.R

--- R/ReCIDE_all_function.R
+++ R/ReCIDE_all_function.R
@@ -950,3 +950,58 @@ cosine_screen_LowToHigh<-function(S,B){
   return(list(list_cos,list_batch))
 }
 
+
+
+compare_group_proportion <- function(results_list,file_re_df){
+  df_category1 = as.data.frame(t(results_list[[1]]))
+  df_category2 = as.data.frame(t(results_list[[2]]))
+  
+  
+  col_inter = intersect(colnames(df_category1),colnames(df_category2))
+  diff1 <- setdiff(colnames(df_category1), col_inter)
+  diff2 <- setdiff(colnames(df_category2), col_inter)
+  if(length(diff1) > 0){df_category2[,diff1] = 0}
+  
+  if(length(diff2) > 0){df_category1[,diff2] = 0}
+  
+  df_category1 = df_category1[,sort(colnames(df_category1))]
+  df_category2 = df_category2[,sort(colnames(df_category2))]
+  
+  p_values <- numeric(length = ncol(df_category1))
+  trends <- character(length = ncol(df_category2))
+  diff <- numeric(length = ncol(df_category2))
+  
+  for (j in seq_along(colnames(df_category1))) {
+    col_name <- colnames(df_category1)[j]
+    
+    df_category1_in = df_category1[, col_name]
+    
+    
+    df_category2_in = df_category2[, col_name]
+    
+    
+    pvalue <- wilcox.test(df_category1_in, df_category2_in, alternative = "two.sided")$p.value
+    p_values[j] <- pvalue
+    diff[j] <- abs(median(df_category1_in)-median(df_category2_in))
+    if (diff[j]==0){
+      diff[j] <- abs(mean(df_category1_in)-mean(df_category2_in))
+    }
+    #   
+    if (median(df_category1_in)<median(df_category2_in)) {
+      trends[j] <- '+'  
+    } else if (median(df_category1_in)>median(df_category2_in)) {
+      trends[j] <- '-'
+    } else {
+      trends[j] <- '0'  
+    }
+  }
+  adjusted_p_values <- p.adjust(p_values, method = "bonferroni")
+  
+  re_df <- data.frame(
+    Celltype = colnames(df_category1),
+    PValue = adjusted_p_values,
+    Trend = trends,
+    Diff=diff
+  )
+  saveRDS(re_df,file=file_re_df)
+}
