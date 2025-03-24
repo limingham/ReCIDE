407d62437a0cedc9d7b65df5e0f2372adc9d73c8
limingham <160384038+limingham@users.noreply.github.com>
2025-03-24 11:30:40 +0800

    Update function_compare_proportion.R

--- R/function_compare_proportion.R
+++ R/function_compare_proportion.R
@@ -1,4 +1,4 @@
-compare_group_proportion <- function(results_list,file_re_df){
+compare_group_proportion <- function(results_list,dir_diff_celltype_output){
   df_category1 = as.data.frame(t(results_list[[1]]))
   df_category2 = as.data.frame(t(results_list[[2]]))
   
@@ -49,7 +49,7 @@ compare_group_proportion <- function(results_list,file_re_df){
     Trend = trends,
     Diff=diff
   )
-  saveRDS(re_df,file=file_re_df)
+  saveRDS(re_df,file=dir_diff_celltype_output)
 }
 
 
